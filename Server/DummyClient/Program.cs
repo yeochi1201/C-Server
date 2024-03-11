c6eac2341a4e14369cae3f507c2f2b871159f04a
박주현 <yeochi1201@gmail.com>
2024-03-12 03:21:27 +0900

    ServerCore

---
+++ Server/DummyClient/Program.cs
@@ -0,0 +1,67 @@
+﻿using ServerCore;
+using System;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace DummyClient
+{
+    class GameSession : Session
+    {
+        public override void OnConnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"On Connected : {endPoint}");
+
+            for(int i = 0; i< 5; i++)
+            {
+                byte[] sendBuff = Encoding.UTF8.GetBytes($"{i} message");
+                Send(sendBuff);
+            }
+        }
+
+        public override void OnDisconnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"On Disconnected : {endPoint}");
+        }
+
+        public override int OnReceive(ArraySegment<byte> buffer)
+        {
+            string recvData = Encoding.UTF8.GetString(buffer.Array, buffer.Offset, buffer.Count);
+            Console.WriteLine($"[From Server] {recvData}");
+            return buffer.Count;
+        }
+
+        public override void OnSend(int numOfBytes)
+        {
+            Console.WriteLine($"Transferred bytes : {numOfBytes}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DNS
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            Connector connector = new Connector();
+            connector.Connect(endPoint, () => { return new GameSession(); });
+
+            while (true)
+            {
+                try
+                {
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
